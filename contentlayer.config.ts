87dd5e06ee31d68178240c03fa2082e08b821914
thisyujeong <this.yujeong@gmail.com>
2022-05-22 21:46:33 +0900

    build: rephype higlight, set contentlayer for note

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -2,26 +2,60 @@ import {
   ComputedFields,
   defineDocumentType,
   makeSource,
+  LocalDocument,
 } from 'contentlayer/source-files';
 import remarkGfm from 'remark-gfm';
 import rehypeSlug from 'rehype-slug';
 import rehypeCodeTitles from 'rehype-code-titles';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypePrism from 'rehype-prism-plus';
+import highlight from 'rehype-highlight';
+import { DocumentGen } from 'contentlayer/core';
 
-const computedFields: ComputedFields = {
+export const computedFields: ComputedFields = {
   slug: {
     type: 'string',
     resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ''),
   },
 };
 
+export const urlFromFilePath = (doc: DocumentGen): string => {
+  return doc._raw.flattenedPath.replace(/pages\/?/, '');
+};
+
+export const Note = defineDocumentType(() => ({
+  name: 'Note',
+  filePathPattern: `note/**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: { type: 'string', required: true },
+    date: { type: 'string', required: true },
+  },
+  computedFields: {
+    url_path: {
+      type: 'string',
+      description:
+        'The URL path of this page relative to site root. For example, the site root page would be "/", and doc page would be "docs/getting-started/"',
+      resolve: urlFromFilePath,
+    },
+    pathSegments: {
+      type: 'json',
+      resolve: (doc) =>
+        doc._raw.flattenedPath
+          .split('/')
+          // skip `/docs` prefix
+          .slice(1)
+          .map((pathName) => {
+            return { pathName };
+          }),
+    },
+  },
+}));
+
 export const Blog = defineDocumentType(() => ({
   name: 'Blog',
-  contentType: 'mdx',
   filePathPattern: `blog/*.mdx`,
+  contentType: 'mdx',
   fields: {
-    // mdx 의 헤더를 정의할 수 있음.
     title: { type: 'string', required: true },
     date: { type: 'string', required: true },
     description: { type: 'string', required: true },
@@ -38,13 +72,13 @@ export const Blog = defineDocumentType(() => ({
 
 export default makeSource({
   contentDirPath: 'posts',
-  documentTypes: [Blog],
+  documentTypes: [Blog, Note],
   mdx: {
     remarkPlugins: [remarkGfm],
     rehypePlugins: [
       rehypeSlug,
       rehypeCodeTitles,
-      rehypePrism,
+      highlight,
       [
         rehypeAutolinkHeadings,
         {
