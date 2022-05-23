34b8157677b0d0a19e893f978ac7201a17d60b8a
thisyujeong <this.yujeong@gmail.com>
2022-05-23 20:14:01 +0900

    style: markdown styling

--- components/Post/MDXPost.style.ts
+++ components/Post/MDXPost.style.ts
@@ -3,11 +3,144 @@ import styled from '@emotion/styled';
 export const MDXPostContainer = styled.div`
   width: 100%;
   font-size: 16px;
-  line-height: 1.57;
+  line-height: 1.8;
+  font-weight: 400;
+
+  * {
+    color: #2c3e50;
+  }
+
+  a {
+    color: #2a75e9;
+    transition: color 0.2s ease;
+    &:hover {
+      color: #289aff;
+    }
+  }
+
+  h1 {
+    font-size: 28px;
+    font-weight: 500;
+    margin: 30px 0 0;
+  }
+
+  h2 {
+    font-size: 26px;
+    font-weight: 500;
+    margin: 30px 0 0;
+  }
+
+  h3 {
+    font-size: 22px;
+    font-weight: 500;
+    margin: 30px 0 0;
+  }
+
+  h4 {
+    font-size: 18px;
+    font-weight: 500;
+    margin: 30px 0 0;
+  }
+
+  h5 {
+    font-size: 17px;
+    font-weight: 500;
+    margin: 30px 0 0;
+  }
+
+  h6 {
+    font-size: 16px;
+    font-weight: 500;
+    margin: 30px 0 0;
+  }
+
+  strong {
+    font-weight: 600;
+  }
+
+  p {
+    margin: 0.7em 0;
+    code {
+      color: #476582;
+      padding: 0.25rem 0.5rem;
+      margin: 0;
+      font-size: 0.85em;
+      background-color: rgba(27, 31, 35, 0.05);
+      border-radius: 3px;
+    }
+  }
+
+  pre {
+    padding: 10px 16px;
+    font-size: 14px;
+    font-weight: 500;
+    border-radius: 5px;
+    background-color: rgba(27, 31, 35, 0.05);
+    font-size: 15px;
+  }
+
+  blockquote {
+    margin: 0.5rem 0;
+    padding: 0.5rem 1rem;
+    position: relative;
+    border-left: 4px solid #dfe2e5;
+
+    & p {
+      display: inline;
+      color: #8a929b;
+    }
+  }
+
+  ul {
+    margin: 0;
+    padding-left: 20px;
+    margin: 1rem 0;
+    li {
+      list-style: disc;
+    }
+  }
+
+  ol {
+    margin: 0;
+    padding-left: 20px;
+    margin: 1rem 0;
+    li {
+      list-style: decimal;
+    }
+  }
+
+  table {
+    margin: 1em 0;
+    border-collapse: collapse;
+    width: auto;
+  }
+
+  thead th {
+    border-bottom: 2px solid #dfe2e5;
+  }
+
+  td {
+    padding: 0.5rem 1.2rem;
+    border: 1px solid #dfe2e5;
+  }
+
+  th {
+    padding: 0.5rem 1.2rem;
+    font-weight: 500;
+    border: 1px solid #dfe2e5;
+  }
+
+  sup a {
+    font-size: 12px;
+  }
+
+  img {
+    max-width: 100%;
+  }
 `;
 
 export const MDXPostTitle = styled.h1`
-  font-size: 28px;
+  font-size: 30px !important;
   word-break: keep-all;
 `;
 
