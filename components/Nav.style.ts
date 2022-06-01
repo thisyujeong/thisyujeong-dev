24f22753fdb57921154fae13b537da8c2e1f4041
thisyujeong <this.yujeong@gmail.com>
2022-06-01 21:17:15 +0900

    feat: add responsive nav

--- components/Nav.style.ts
+++ components/Nav.style.ts
@@ -1,7 +1,10 @@
 import styled from '@emotion/styled';
+import mq from 'styles/utils/mq';
+import { css } from '@emotion/react';
 
-export const NavContainer = styled.div`
+export const NavContainer = styled.nav`
   margin-left: 24px;
+  line-height: 1;
 
   ul {
     display: flex;
@@ -33,5 +36,59 @@ export const NavContainer = styled.div`
         color: var(--accent);
       }
     }
+
+    ${mq({
+      display: ['flxex', 'flex', 'none'],
+    })}
   }
+
+  svg {
+    opacity: 0.8;
+    margin-right: -4px;
+
+    &.active {
+      opacity: 1;
+    }
+    ${mq({
+      display: ['none', 'none', 'block'],
+    })}
+  }
+`;
+
+const dynamicVisible = ({ visible }: { visible: boolean }) => {
+  if (visible) {
+    return css`
+      display: flex;
+    `;
+  } else {
+    return css`
+      display: none;
+    `;
+  }
+};
+
+export const DropNavContainer = styled.nav`
+  position: fixed;
+  top: 50px;
+  right: 0;
+  background: #fff;
+  width: 100%;
+  height: calc(100vh - 50px);
+  text-align: left;
+
+  ul {
+    width: 100%;
+    display: block;
+    padding: 22px;
+
+    li {
+      font-size: 24px;
+      margin-bottom: 16px;
+
+      a {
+        display: block;
+      }
+    }
+  }
+  ${dynamicVisible}
 `;
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,14 @@
-import { NavContainer } from './Nav.style';
+import { NavContainer, DropNavContainer } from './Nav.style';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import React from 'react';
+import React, { Dispatch, SetStateAction, useState } from 'react';
 import navLinks from '../data/navLinks';
 
-const Links = () => {
+const Links = ({
+  setIsOpenMenu,
+}: {
+  setIsOpenMenu: Dispatch<SetStateAction<boolean>>;
+}) => {
   const router = useRouter();
 
   return (
@@ -15,7 +19,7 @@ const Links = () => {
           data-selected={router.pathname.includes(link.path) ? true : false}
         >
           <Link href={link.link} passHref>
-            <a>{link.title}</a>
+            <a onClick={() => setIsOpenMenu(false)}>{link.title}</a>
           </Link>
         </li>
       ))}
@@ -24,11 +28,32 @@ const Links = () => {
 };
 
 const Nav = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false);
+
   return (
     <>
       <NavContainer>
-        <Links />
+        <Links setIsOpenMenu={setIsOpenMenu} />
+        <svg
+          className={isOpenMenu ? 'active' : ''}
+          onClick={() => setIsOpenMenu(!isOpenMenu)}
+          xmlns="http://www.w3.org/2000/svg"
+          width="28"
+          height="28"
+          viewBox="0 0 20 20"
+        >
+          <g transform="matrix(-1,0,0,1,20,0)">
+            <path
+              xmlns="http://www.w3.org/2000/svg"
+              d="m3 5c0-.26522.10536-.51957.29289-.70711.18754-.18753.44189-.29289.70711-.29289h12c.2652 0 .5196.10536.7071.29289.1875.18754.2929.44189.2929.70711s-.1054.51957-.2929.70711c-.1875.18753-.4419.29289-.7071.29289h-12c-.26522 0-.51957-.10536-.70711-.29289-.18753-.18754-.29289-.44189-.29289-.70711zm0 5c0-.26522.10536-.51957.29289-.70711.18754-.18753.44189-.29289.70711-.29289h6c.2652 0 .5196.10536.7071.29289.1875.18754.2929.44189.2929.70711 0 .2652-.1054.5196-.2929.7071s-.4419.2929-.7071.2929h-6c-.26522 0-.51957-.1054-.70711-.2929-.18753-.1875-.29289-.4419-.29289-.7071zm0 5c0-.2652.10536-.5196.29289-.7071.18754-.1875.44189-.2929.70711-.2929h12c.2652 0 .5196.1054.7071.2929s.2929.4419.2929.7071-.1054.5196-.2929.7071-.4419.2929-.7071.2929h-12c-.26522 0-.51957-.1054-.70711-.2929-.18753-.1875-.29289-.4419-.29289-.7071z"
+              fill="#2c3e50"
+            ></path>
+          </g>
+        </svg>
       </NavContainer>
+      <DropNavContainer visible={isOpenMenu}>
+        <Links setIsOpenMenu={setIsOpenMenu} />
+      </DropNavContainer>
     </>
   );
 };
