55ff440c179ae0c1957a00ea8d5b243637491746
thisyujeong <this.yujeong@gmail.com>
2022-06-16 22:59:33 +0900

    style: change home desc

--- data/metadata.ts
+++ data/metadata.ts
@@ -8,7 +8,7 @@ const metadata = {
   author: 'thisyujeong',
   home: {
     name: 'Yujeong Lee',
-    description: '하고자 하는 것이 있다면, 일단 경험해보고 좋으면 계속합니다.',
+    description: '일단 시작하고 좋으면 계속한다는 말을 좋아합니다.',
     // description: 'I experience it and if i like it, i continue.',
     subDescription:
       '웹 퍼블리셔로 일하고 있습니다. 프론트엔드 개발자가 되기 위해 공부하고 있습니다.',
