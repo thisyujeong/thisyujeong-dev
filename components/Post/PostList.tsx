eafe0af20899a5c61c3717e5e58460d69a9c5d9f
thisyujeong <this.yujeong@gmail.com>
2023-12-17 14:22:02 +0900

    feat: 포스트 리스트 검색 컴포넌트 수정, debounce 커스텀 훅 추가

--- components/Post/PostList.tsx
+++ components/Post/PostList.tsx
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from 'react';
 import { Blog } from 'contentlayer/generated';
 import PostCard from './PostCard';
 import SeriesTab from './SeriesTab';
+import Search from 'components/Post/Search';
+import useDebounce from '@/hooks/debounce';
+import styles from './PostList.module.scss';
 
 type Props = {
   posts: Blog[];
@@ -11,31 +14,52 @@ type Props = {
 
 const PostList = ({ posts, series }: Props) => {
   const [selectSeries, setSelectSeries] = useState<string>('');
-  const data =
-    selectSeries !== '' ? posts.filter((post) => post.series === selectSeries) : posts;
+  const [keyword, setKeyword] = useState<string>('');
+  const debounceKeyword = useDebounce(keyword);
+
+  const data = debounceKeyword
+    ? posts.filter((p) => p.title.toLowerCase().includes(debounceKeyword.toLowerCase()))
+    : selectSeries
+    ? posts.filter((post) => post.series === selectSeries)
+    : posts;
 
   const onClickSeries = (series: string) => {
-    if (selectSeries === series) {
-      localStorage.removeItem('series');
+    /** 이미 선택된 시리즈 탭 선택 시 해제 */
+    if (selectSeries !== series) {
+      setSelectSeries(series);
+      localStorage.setItem('series', series);
+    } else {
       setSelectSeries('');
-      return;
+      localStorage.removeItem('series');
     }
+  };
 
-    setSelectSeries(series);
-    localStorage.setItem('series', series);
+  const onChange = (value: string) => {
+    setKeyword(value);
+
+    /** 검색어 입력 시 시리즈 탭 선택 해제 */
+    if (value !== '') {
+      setSelectSeries('');
+      localStorage.removeItem('series');
+    }
   };
 
   useEffect(() => {
-    const localSeries = localStorage.getItem('series');
-    setSelectSeries(localSeries || '');
+    setSelectSeries(localStorage.getItem('series') || '');
   }, []);
 
   return (
     <>
+      <Search onChange={onChange} />
       {<SeriesTab series={series} selected={selectSeries} onClick={onClickSeries} />}
-      {data.map((post, idx) => (
-        <PostCard post={post} key={idx} slug={post.slug} />
-      ))}
+      {data.length ? (
+        data.map((post, idx) => <PostCard post={post} key={idx} slug={post.slug} />)
+      ) : (
+        <div className={styles.empty}>
+          <p className={styles.empty_sub_text}>No search results found.</p>
+          <p className={styles.empty_text}>검색 결과가 없습니다.</p>
+        </div>
+      )}
     </>
   );
 };
