89fade4cbcad7845a1e33fb0513eaa316fcabde5
thisyujeong <this.yujeong@gmail.com>
2023-12-17 19:58:44 +0900

    styles: 테마 토글 시 트랜지션 반영

--- src/styles/utils/mq.js
+++
@@ -1,9 +0,0 @@
-import facepaint from 'facepaint';
-
-const breakpoints = [1280, 750, 576];
-const mq = facepaint(
-  breakpoints.map((bp) => `@media (max-width: ${bp}px)`),
-  { overlap: false }
-);
-
-export default mq;
