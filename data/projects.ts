cddb84f49984158fbf3163aae538cd3fcb59014f
thisyujeong <this.yujeong@gmail.com>
2024-09-26 16:06:06 +0900

    chore: 프로젝트 URL 변경

--- data/projects.ts
+++ data/projects.ts
@@ -15,7 +15,7 @@ const projects: Project[] = [
     title: 'Wave Plane',
     description: 'React Three Fiber(three.js) + GSAP 를 활용한 인터랙티브 웹(2)',
     image: '/projects/wave-plane.png',
-    url: `${process.env.NEXT_PUBLIC_PROJECT_WAVE_PLANE}`,
+    url: 'https://wave-plane-threejs.vercel.app',
   },
   {
     title: 'thisyujeong.dev',
